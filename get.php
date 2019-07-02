377d59e89642df1b9d3f5c291c666186dc535bff
ahsspain <39710743+ahsspain@users.noreply.github.com>
2019-07-02 12:00:39 +0200

    Update get.php

--- get.php
+++ get.php
@@ -1,12 +1,5 @@
 
-<?php  
-$url="https://admiring-engelbart-adafff.netlify.com/"; // url de la pagina que queremos obtener  
-$url_content = '';  
-$file = @fopen($url, 'r');  
-if($file){  
-  while(!feof($file)) {  
-    $url_content .= @fgets($file, 4096);  
-  }  
-  fclose ($file);  
-}  
-?>  
+<?php
+$page = file_get_contents('http://forosdelweb.com');
+echo $page;
+?>
